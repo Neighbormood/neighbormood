202fe7fdec740860806950a90acd49050b9a6bd1
Caruychen <caruychen@gmail.com>
2020-11-07 15:21:23 +0200

    route to home view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <v-app>
     <v-main>
-      <Home />
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Home from "./views/Home";
 
 export default {
   name: "App",
 
   components: {
-    Home
   },
 
   data: () => ({
