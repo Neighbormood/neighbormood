2f5704c48ca9f6547768606037bc87c8440cd525
Caruychen <caruychen@gmail.com>
2020-11-07 15:30:44 +0200

    navbar

--- client/src/components/TheNavigation.vue
+++ client/src/components/TheNavigation.vue
@@ -1,17 +1,27 @@
 <template>
   <v-bottom-navigation app color="primary">
-    <div class="d-flex align-center">
-      <router-link to="/friends">Friends</router-link>
-      <router-link to="/user"><b-icon-house></b-icon-house></router-link>
-      <router-link to="/statistics">Stats</router-link>
+    <div class="d-flex justify-space-around align-center">
+      <router-link to="/friends">
+        <v-icon>
+          mdi-account-group
+        </v-icon>
+      </router-link>
+      <router-link to="/user">
+        <v-icon>
+          mdi-home
+        </v-icon>
+      </router-link>
+      <router-link to="/statistics">
+        <v-icon>
+            mdi-chart-bar
+        </v-icon>
+      </router-link>
     </div>
   </v-bottom-navigation>
 </template>
 
 <style scoped>
-.nav {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
+.d-flex {
+    width: 100%
 }
 </style>
