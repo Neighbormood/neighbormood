71770bf966d81da2ffb4cf9b372fdda74dbc4a0a
Jan Studený <jendas1@yahoo.com>
2020-11-07 17:36:39 +0200

    x

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 # app.py
 from flask import Flask, request, jsonify
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 
@@ -9,7 +8,9 @@ app = Flask(__name__)
 app.config.from_object(Config)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
 connection = engine.connect()
-
+# /me
+# /me/stats
+# /me/
 # A welcome message to test our server
 @app.route('/')
 def index():
