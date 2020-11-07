614fdc7a4a8533215e2d370c8efc32f7e8294e75
Caruychen <caruychen@gmail.com>
2020-11-07 14:36:11 +0200

    temporary login link

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,3 +1,7 @@
 <template>
-    <div id="login-view"></div>
+    <div id="login-view">
+        <h1>Login page</h1>
+        <!-- Temporary login link -->
+        <router-link to="/">login</router-link>
+    </div>
 </template>
