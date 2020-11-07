fd245e3bc54fd99d752136da0285a5672504f200
Caruychen <caruychen@gmail.com>
2020-11-07 14:59:57 +0200

    added axios

--- client/src/components/TheNavigation.vue
+++ client/src/components/TheNavigation.vue
@@ -1,7 +1,15 @@
 <template>
   <nav class="nav" id="nav">
-    <router-link to="/">User</router-link> |
-    <router-link to="/statistics">Stats</router-link> |
-    <router-link to="/friends">Friends</router-link>
+    <router-link to="/friends"></router-link>
+    <router-link to="/user"><b-icon-house></b-icon-house></router-link>
+    <router-link to="/statistics">Stats</router-link>
   </nav>
 </template>
+
+<style scoped>
+.nav {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
