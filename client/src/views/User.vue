39a09b147fd228d1ffc34d6f03dc0a0622f1d95a
Hai Phan <hatazhix@gmail.com>
2020-11-07 20:18:21 +0200

    Date and time picker complete

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -2,6 +2,61 @@
   <div id="user-view" class="container">
     <h1 class="title">Today</h1>
     <h1 class="title">{{ today }}</h1>
+    <!-- Date picker -->
+    <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-right="40"
+      transition="scale-transition"
+      offset-y
+      min-width="290px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          v-model="user.date"
+          label="Picker without buttons"
+          prepend-icon="mdi-calendar"
+          readonly
+          v-bind="attrs"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-date-picker
+        v-model="user.date"
+        @input="menu = false"
+        @click="$refs.menu.save(date)"
+      ></v-date-picker>
+    </v-menu>
+    <!-- Time picker -->
+    <v-menu
+      ref="menu"
+      v-model="menu2"
+      :close-on-content-click="false"
+      :nudge-right="40"
+      :return-value.sync="time"
+      transition="scale-transition"
+      offset-y
+      max-width="290px"
+      min-width="290px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          v-model="user.time"
+          label="Picker in menu"
+          prepend-icon="mdi-clock-time-four-outline"
+          readonly
+          v-bind="attrs"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-time-picker
+        v-if="menu2"
+        v-model="user.time"
+        full-width
+        format="24h"
+        @click:minute="$refs.menu.save(user.time)"
+      ></v-time-picker>
+    </v-menu>
     <v-divider />
 
     <v-subheader class="pl-0">
@@ -20,7 +75,7 @@
     </v-slider>
 
     <p class="centerText">I am feeling {{ moodText(user.moodScore) }}</p>
-    <p>Mood score: {{ user.moodScore }}</p>
+    <!-- <p>Mood score: {{ user.moodScore }}</p> -->
 
     <v-textarea
       auto-grow
@@ -29,11 +84,17 @@
       label="Describe your mood"
       rows="1"
       :value="user.moodDescription"
-      @input="updateValue($event)"
+      @input="updateMoodText($event)"
       ref="input"
     ></v-textarea>
     <div class="centerText">
-      <v-btn color="primary" large elevation="2" outlined rounded
+      <v-btn
+        color="primary"
+        large
+        elevation="2"
+        outlined
+        rounded
+        @click="sendMood()"
         >Save Mood</v-btn
       >
     </div>
@@ -47,10 +108,21 @@ export default {
   data() {
     return {
       user: {
-        uid: "Get uid",
+        uid: "UID",
         moodScore: 3,
-        moodDescription: ""
+        moodDescription: "",
+        date: new Date().toISOString().substr(0, 10),
+        time: moment().format("HH:mm")
       },
+
+      /* Date picker */
+      date: new Date().toISOString().substr(0, 10),
+      menu: false,
+
+      /* Time picker */
+      menu2: false,
+      modal2: false,
+
       today: moment().format("DD.MM.YYYY HH:mm"),
       satisfactionEmojis: ["😭", "😢", "🙁", "😐", "🙂", "😊", "😍"],
       moodLabels: [
@@ -74,9 +146,21 @@ export default {
       return this.moodLabels[userMoodScore];
     },
 
-    updateValue(textMood) {
+    updateMoodText(textMood) {
       this.user.moodDescription = textMood;
       console.log(this.user.moodDescription);
+    },
+    sendMood() {
+      var obj = {
+        user: {
+          uid: this.user.uid,
+          moodScore: this.user.moodScore,
+          moodDescription: this.user.moodDescription,
+          date: this.user.date,
+          time: this.user.time
+        }
+      };
+      console.log("Data to send: " + JSON.stringify(obj));
     }
   }
 };
@@ -91,5 +175,10 @@ export default {
 }
 .centerText {
   text-align: center;
+}
+.v-slider--horizontal {
+  min-height: 32px;
+  margin-left: 8px;
+  margin-right: 16px;
 }
 </style>
