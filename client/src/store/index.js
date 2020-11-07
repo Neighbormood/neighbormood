f1aa822761b7ed87be5002ddf93e3beaabb541e6
Caruychen <caruychen@gmail.com>
2020-11-07 17:47:07 +0200

    friends view

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import user from "./modules/user";
-
+import friends from "./modules/friends"
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -9,6 +9,7 @@ export default new Vuex.Store({
   mutations: {},
   actions: {},
   modules: {
-    user
+    user,
+    friends
   }
 });
